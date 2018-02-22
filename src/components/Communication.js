04700be124331e232208d1e6019f518786db0237
lojdquist <axel.lojdquist@hotmail.com>
2018-02-22 09:42:13 +0100

    Add unique id

--- src/components/Communication.js
+++ src/components/Communication.js
@@ -8,23 +8,23 @@ class Communication extends Component {
     this.client = createDeepstream('10.90.128.65:60020').login();
     // Login
     this.counter = 0;
-    this.instance = 'abc';
+    this.instance = this.client.getUid();
+    this.players = [];
     this.addPlayer = this.addPlayer.bind(this);
     this.readSensorData = this.readSensorData.bind(this);
 
     this.client.rpc.provide(`data/${this.instance}/addPlayer`, this.addPlayer);
-    this.heartbeatCounter = 0;
     this.client.event.subscribe(`data/${this.instance}/${this.counter}`, this.readSensorData);
 
     this.state = {
       beta: 0,
-      gamma: 0
+      gamma: 0,
     };
   }
 
   addPlayer(data, response) {
-    this.counter += 1;
-    this.client.event.subscribe(`data/${this.instance}/${this.counter}`, this.readSensorData);
+    this.players.push({ id: data.id, name: data.name });
+    this.client.event.subscribe(`data/${this.instance}/${this.data.id}`, this.readSensorData);
     response.send(this.counter);
   }
 
@@ -32,7 +32,7 @@ class Communication extends Component {
     console.log('asd');
     this.setState({
       beta: data.beta,
-      gamma: data.gamma
+      gamma: data.gamma,
     });
   }
 
