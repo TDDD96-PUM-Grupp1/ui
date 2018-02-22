4456af3ce0aae9eab935885d95427c5019a8a2e0
Alexander Wilkens <alewi684@student.liu.se>
2018-02-22 16:33:05 +0100

    Remove header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 import Communication from './components/Communication';
 import PlayerList from './components/PlayerList';
 import StartMenu from './components/StartMenu';
@@ -9,16 +8,8 @@ const com = new Communication();
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
     <StartMenu />
     <PlayerList getPlayersFunction={com.getPlayers} />
-
   </div>
 );
 
