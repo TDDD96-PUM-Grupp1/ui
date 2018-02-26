3850a48c2ac2ce874fc850296ba24df7ac7749df
Lojdquist <axel.lojdquist@hotmail.com>
2018-02-26 10:36:23 +0100

    Communication (#7)
    * Add config files for eslint and prettier
    
    * Add default src and public folder for react, together with package.json
    
    * Update README
    
    * Change name of package
    
    * Update .travis.yml
    
    * Add yarn lock file
    
    * add start-pc script for windows
    
    * add basefunctionallity for deepstream
    
    * Add unique id
    
    * change datastructure
    
    * Add rule to eslint config to fix issue with trailing comma warning.
    
    * add comments to methods, removes players from list when looses connection
    
    * add presence
    
    * commented out presence functionallity
    
    * Add player list
    
    * Add proptype dependency
    
    * Add playerlist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './css/App.css';
-
 import StartMenu from './components/StartMenu';
+import Communication from './components/Communication';
+import PlayerList from './components/PlayerList';
+
+function getDummyName() {
+  return {
+    'cool-id': {
+      name: 'my-name',
+      sensor: {
+        beta: 5,
+        gamma: 7,
+      },
+    },
+  };
+}
+
+const com = new Communication();
 
 const App = () => (
   <div className="App">
     <StartMenu />
+    <PlayerList getPlayersFunction={getDummyName} />
   </div>
 );
 
