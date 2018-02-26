10ce3d7b382d461ec1ececc884fb0dbdff728285
Alexander Wilkens <alewi684@student.liu.se>
2018-02-26 10:46:02 +0100

    Fix PIXI rendering

--- src/components/Gametest.js
+++ src/components/Gametest.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import PIXI from 'pixi.js';
+import * as PIXI from 'pixi.js';
 
 /*
 Menu in UI with interchangeable content.
@@ -9,11 +9,26 @@ class Gametest extends Component {
   constructor(props) {
     super(props);
 
-    this.renderer = new PIXI.Application({ width: 256, height: 256 });
+    this.setRef = this.setRef.bind(this);
+  }
+
+  componentDidMount() {
+    // Setup PIXI Canvas in componentDidMount
+    this.renderer = PIXI.autoDetectRenderer(1366, 768);
+    this.refElement.appendChild(this.renderer.view);
+
+    // create the root of the scene graph
+    this.stage = new PIXI.Container();
+    this.stage.width = 1366;
+    this.stage.height = 768;
+  }
+
+  setRef(c) {
+    this.refElement = c;
   }
 
   render() {
-    return <div className="game">{this.renderer.view}</div>;
+    return <div className="game-canvas-container" ref={this.setRef} />;
   }
 }
 
