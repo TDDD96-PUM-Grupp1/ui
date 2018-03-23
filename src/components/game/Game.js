4be239faa03f4857f491f44f346eab91d062260d
Alexander Wilkens <alewi684@student.liu.se>
2018-03-23 09:05:19 +0100

    Remove unused variable

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -3,20 +3,12 @@ import EntityHandler from './EntityHandler';
 import TestGamemode from './gamemodes/TestGamemode';
 import CollisionHandler from './CollisionHandler';
 
-/* eslint-disable */
-var GAME;
-/* eslint-enable */
-
 /*
 Game.
 */
 class Game {
   constructor(app) {
     this.app = app;
-
-    // Set global variables for the pixi context and game context to heavily simplify
-    // the creation of additional objects and graphics when necessary.
-    GAME = this;
 
     // Create all handlers
     this.entityHandler = new EntityHandler();
