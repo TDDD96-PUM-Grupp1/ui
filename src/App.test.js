21491fbc54c875245d22b5f0e75abbd138d3ca98
Alexander Wilkens <alewi684@student.liu.se>
2018-03-26 16:58:37 +0200

    Add some tests for resource server

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import fs from 'file-system';
 import App from './App';
+import ResourceServer from './game/ResourceServer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('has correct error on missing resource', async () => {
+  const filenames = [];
+
+  const errorName = 'hejsan';
+  const errorFilepath = 'NO_EXIST.txt';
+
+  filenames.push({
+    name: errorName,
+    path: errorFilepath,
+  });
+
+  expect.assertions(1);
+
+  const rs = new ResourceServer();
+
+  await expect(rs.requestResources(filenames)).rejects.toEqual(
+    new Error(`Failed to load resource ${errorName} from path resources/${errorFilepath}`),
+  );
+});
+
+it('loads correct resources', async () => {
+  const filenames = [];
+  const outputObject = {};
+  fs.recurseSync('./public/resources', (filepath, relative, filename) => {
+    filenames.push({
+      name: 'hello',
+      path: filename,
+    });
+
+    outputObject[filename] = 'DOESNT_MATTER';
+  });
+
+  expect.assertions(1);
+  const rs = new ResourceServer();
+
+  await expect(rs.requestResources(filenames)).resolves.toMatch(outputObject);
 });
