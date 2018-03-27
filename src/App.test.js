60fb213992b1d5dabf0e29c4206cf95debe8c47d
Alexander Wilkens <alewi684@student.liu.se>
2018-03-27 11:56:51 +0200

    Continue with figuring out resource test

--- src/App.test.js
+++ src/App.test.js
@@ -22,28 +22,30 @@ it('has correct error on missing resource', async () => {
   });
 
   expect.assertions(1);
-
   const rs = new ResourceServer();
-
   await expect(rs.requestResources(filenames)).rejects.toEqual(
     new Error(`Failed to load resource ${errorName} from path resources/${errorFilepath}`),
   );
 });
 
-it('loads correct resources', async () => {
-  const filenames = [];
-  const outputObject = {};
-  fs.recurseSync('./public/resources', (filepath, relative, filename) => {
-    filenames.push({
-      name: 'hello',
-      path: filename,
-    });
+// it(
+//   'loads correct resources',
+//   () => {
+//     const filenames = [];
+//     const outputObject = {};
+//     fs.recurseSync('./public/resources', (filepath, relative, filename) => {
+//       filenames.push({
+//         name: 'hello',
+//         path: filename,
+//       });
 
-    outputObject[filename] = 'DOESNT_MATTER';
-  });
+//       outputObject[filename] = 'DOESNT_MATTER';
+//     });
 
-  expect.assertions(1);
-  const rs = new ResourceServer();
-
-  await expect(rs.requestResources(filenames)).resolves.toMatch(outputObject);
-});
+//     // expect.assertions(1);
+//     const rs = new ResourceServer();
+//     // rs.requestResources(filenames).then(msg => console.log(msg));
+//     return expect(rs.requestResources(filenames)).resolves.toMatch(outputObject);
+//   },
+//   15000,
+// );
