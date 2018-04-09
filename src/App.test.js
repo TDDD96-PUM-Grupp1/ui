bbd367370ef80ca06ba486cc6f113f836ff5828c
joelnir <joel.oscarsson@hotmail.com>
2018-04-09 14:14:52 +0200

    Remove broken test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import fs from 'file-system';
 import App from './App';
 import ResourceServer from './game/ResourceServer';
 
@@ -23,29 +22,9 @@ it('has correct error on missing resource', async () => {
 
   expect.assertions(1);
   const rs = new ResourceServer();
+  /* eslint-disable function-paren-newline */
   await expect(rs.requestResources(filenames)).rejects.toEqual(
     new Error(`Failed to load resource ${errorName} from path resources/${errorFilepath}`),
   );
+  /* eslint-enable function-paren-newline */
 });
-
-// it(
-//   'loads correct resources',
-//   () => {
-//     const filenames = [];
-//     const outputObject = {};
-//     fs.recurseSync('./public/resources', (filepath, relative, filename) => {
-//       filenames.push({
-//         name: 'hello',
-//         path: filename,
-//       });
-
-//       outputObject[filename] = 'DOESNT_MATTER';
-//     });
-
-//     // expect.assertions(1);
-//     const rs = new ResourceServer();
-//     // rs.requestResources(filenames).then(msg => console.log(msg));
-//     return expect(rs.requestResources(filenames)).resolves.toMatch(outputObject);
-//   },
-//   15000,
-// );
