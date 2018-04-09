6b8f0477eecacab5adecc766d5fae33e502a360a
Joel Oskarsson <joel.oscarsson@hotmail.com>
2018-04-09 15:51:56 +0200

    Add Resource Server (#16)
    * Add resource server

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,6 +1,7 @@
 // import * as PIXI from 'pixi.js';
 import EntityHandler from './EntityHandler';
 import CollisionHandler from './CollisionHandler';
+import ResourceServer from './ResourceServer';
 import GamemodeHandler from './GamemodeHandler';
 
 /*
@@ -15,6 +16,7 @@ class Game {
     // Create all handlers
     this.entityHandler = new EntityHandler();
     this.collisionHandler = new CollisionHandler(this.entityHandler);
+    this.resourceServer = new ResourceServer();
 
     // Create gamemode
     const gamemodeHandler = GamemodeHandler.getInstance();
