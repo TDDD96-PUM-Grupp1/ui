167b28de110825ce1b74e620d33f665877ce27b7
joelnir <joel.oscarsson@hotmail.com>
2018-04-12 14:34:38 +0200

    Add more documentation to scoremanager

--- src/game/ScoreManager.js
+++ src/game/ScoreManager.js
@@ -35,6 +35,10 @@ class ScoreManager {
     this.resort();
   }
 
+  /*
+  Add a new score type to be part of score tracking
+  If primary is true the score list is sorted by this score value
+  */
   addScoreType(name, defaultVal, primary = false){
     this.defaultScores[name] = defaultVal;
 
@@ -53,19 +57,31 @@ class ScoreManager {
   Sort highscorelist based on the current orderPolicy and primaryScore
   */
   resort(){
+    if(this.primaryScore === ''){
+      return;
+    }
+
     this.highscoreList.sort((a, b) => this.orderPolicy(a[this.primaryScore], b[this.primaryScore]));
     this.triggerUpdate();
   }
 
+  /*
+  Get the highscore list sorted on the primary score type
+  */
   getList(){
     return this.highscoreList;
   }
 
-
+  /*
+  Add an object with an update() function to be called every time a score is updated
+  */
   addScoreListener(listener){
     this.scoreListeners.push(listener);
   }
 
+  /*
+  Add a new player to keep track of score for
+  */
   addPlayer(idTag){
     let newObj = {
       'id': idTag,
@@ -81,10 +97,17 @@ class ScoreManager {
     this.resort();
   }
 
+  /*
+  Remove a player to not keep counting score for
+  */
   removePlayer(idTag){
     delete this.scores[idTag];
   }
 
+  /*
+  Reset all scores of the given type to their default values
+  if idTag is given only the player with the given tag has their score reset
+  */
   resetScore(scoreType, idTag = undefined) {
     this.highscoreList.forEach((val, index) => {
       this.highscoreList[index].score = 0;
@@ -93,6 +116,9 @@ class ScoreManager {
     this.triggerUpdate();
   }
 
+  /*
+  Helper function to mutate a score according to a given function from current score to new score
+  */
   mutateScore(scoreType, idTag, muteFunc){
     if (! (scoreType in this.defaultScores)){
       throw new Error(scoreType + ' not a valid scoretype');
@@ -111,10 +137,17 @@ class ScoreManager {
     }
   }
 
+  /*
+  Add n points to the given score
+  */
   addScore(scoreType, idTag, n){
     this.mutateScore(scoreType, idTag, x => x + n);
   }
 
+  /*
+  Remove n points to the given score
+  If allowNegative is true the score is allowed to go below 0
+  */
   removeScore(scoreType, idTag, n, allowNegative = false){
     if(allowNegative){
       this.mutateScore(scoreType, idTag, x => x - n);
@@ -124,6 +157,9 @@ class ScoreManager {
     }
   }
 
+  /*
+  Set the given score to n
+  */
   setScore(scoreType, idTag, n){
     this.mutateScore(scoreType, idTag, x => n);
   }
