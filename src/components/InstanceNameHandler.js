37717cf2287655b40da7045669f06e834c7b8341
Tim Hakansson <timha404@liu.se>
2018-04-12 17:14:52 +0200

    Add random instance name

--- src/components/InstanceNameHandler.js
+++ src/components/InstanceNameHandler.js
@@ -1,57 +1,52 @@
-// var fs = require('fs');
+const nouns = [
+  "Alchemist",
+  "Amulet",
+  "Apprentice",
+  "Beast",
+  "Beauty",
+  "Bersek",
+  "Bogeyman",
+  "Brew",
+  "Castle",
+  "Cauldron",
+  "Cave",
+  "Chalice",
+  "Creature",
+  "Crow",
+  "Curse",
+];
 
-class InstanceNameHandler {
-  // This constructor will be used when we implement the random name generator.
-  /* eslint-disable no-useless-constructor */
-  constructor() {
-    /* eslint-enable no-useless-constructor */
-    /*
-    // Load in nouns
-    fs.readFile('InstanceWordsNouns.txt', (err, data) => {
-      if (err) throw err;
-      this.nouns = `${data}`.split('\n');
-      for (let i = this.nouns.length - 1; i >= 0; i -= 1) {
-        // Sometimes there are empty strings in the list. Remove them
-        if (this.nouns[i] === '') {
-          this.nouns.splice(i, 1);
-        }
-      }
-    });
+const adjectives = [
+ "Abnormal",
+ "Apprentice",
+ "Awful",
+ "Beautiful",
+ "Berserk",
+ "Bewitched",
+ "Bizarre",
+ "Captivated",
+ "Charismatic",
+ "Charming",
+ "Cruel",
+ "Dancing",
+ "Delirious",
+ "Dramatic",
+ "Enchanting",
+ "Evil",
+ "Familiar",
+ "Fantastic",
+ "Fascinating",
+ "Fierce",
+];
 
-    // Load in adjectives
-    fs.readFile('InstanceWordsAdjectives.txt', (err, data) => {
-      if (err) throw err;
-      this.adjectives = `${data}`.split('\n');
-      for (let i = this.adjectives.length - 1; i >= 0; i -= 1) {
-        // Sometimes there are empty strings in the list. Remove them
-        if (this.adjectives[i] === '') {
-          this.adjectives.splice(i, 1);
-        }
-      }
-    });
-    this.getRandomItemFromList = this.getRandomItemFromList.bind(this);
-    this.getRandomInstaneName = this.getRandomInstanceName.bind(this);
-    */
-  }
-
-  /*
-   * returns a random item from a given list.
-   */
-  /* eslint-disable class-methods-use-this */
-  getRandomItemFromList(list) {
-    /* eslint-enable class-methods-use-this */
-
-    return list[Math.floor(Math.random() * list.length)];
-  }
-
-  /*
-   * returns a random instance name from a list of adjectives and nouns.
-   */
-  /* eslint-disable class-methods-use-this */
-  getRandomInstanceName() {
-    /* eslint-enable class-methods-use-this */
-    return 'FierceCastle'; // this.getRandomItemFromList(this.adjectives) + this.getRandomItemFromList(this.nouns);
-  }
+function getRandomFromList(list)
+{
+  return list[Math.floor(Math.random() * list.length)];
+}
+// eslint-disable-next-line
+function getRandomInstanceName()
+{
+  return getRandomFromList(nouns) + getRandomFromList(adjectives);
 }
 
-export default InstanceNameHandler;
+export default getRandomInstanceName;
