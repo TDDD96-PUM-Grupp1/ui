935283ba163d44293f085f61a74d9ffab39ad9da
Joel Almqvist <joeal360@student.liu.se>
2018-04-13 18:59:54 +0200

    Add randomly changing map sie

--- src/game/gamemodes/KnockOff.js
+++ src/game/gamemodes/KnockOff.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import * as PIXI from 'pixi.js';
 import Gamemode from './Gamemode';
 
@@ -6,6 +7,15 @@ const RESPAWN_TIME = 3;
 
 // The max time between a collision and a player dying in order to count as a kill.
 const TAG_TIME = 4;
+
+// Constants for self-changing gameboard size
+const maxRadius = 500;
+const minRadius = 200;
+const smallDec = 0.95;
+const largeDec = 0.9;
+const smallInc = 1.05;
+const largeInc = 1.1;
+const changeInterval = 60;
 
 /*
   Knock off gamemode, get score by knocking other players off the arena.
@@ -18,6 +28,7 @@ class KnockOff extends Gamemode {
     this.respawn = {};
     this.score = {};
     this.tags = {};
+    this.changeCounter = changeInterval;
 
     this.game.respawnHandler.registerRespawnListener(this);
 
@@ -28,7 +39,7 @@ class KnockOff extends Gamemode {
     // Set up arena graphic
     const graphic = new PIXI.Graphics();
     graphic.beginFill(0xfffffff);
-    graphic.drawCircle(0, 0, this.arenaRadius);
+    this.mainCircle = graphic.drawCircle(0, 0, this.arenaRadius);
     graphic.endFill();
     game.app.stage.addChildAt(graphic, 0); // Set arena to be first thing to render
     graphic.tint = 0x555555;
@@ -40,15 +51,15 @@ class KnockOff extends Gamemode {
   /* eslint-disable no-unused-vars, class-methods-use-this */
   // Called before the game objects are updated.
   preUpdate(dt) {
-    //this.arenaRadius = Math.floor(Math.random() * (151) + 150);
-/*
-    this.arenaGraphic.beginFill(0xfffffff);
-    this.arenaGraphic.drawCircle(0, 0, this.arenaRadius);
-    this.arenaGraphic.endFill();
-    this.arenaGraphic.tint = 0x555555;
-    this.arenaGraphic.x = this.arenaCenterx;
-    this.arenaGraphic.y = this.arenaCentery;
-*/
+    // TODO make this a new gamemode
+    // TODO make a gamemode where the circle shape changes non randomly
+    if (this.changeCounter <= 0) {
+      this.randomizeCircle();
+      this.changeCounter = changeInterval;
+    } else {
+      this.changeCounter -= 1;
+    }
+
     // Update tags
     Object.keys(this.tags).forEach(id => {
       const list = this.tags[id];
@@ -151,6 +162,44 @@ class KnockOff extends Gamemode {
       this.game.respawnHandler.addRespawn(entity, RESPAWN_TIME);
     } else {
       this.game.entityHandler.unregisterFully(entity);
+    }
+  }
+
+  /**
+   * Randomly resizes the gamecircle, the radius changes with multiplication
+   * and hence the circle's area may changes quite drastically on a single tick.
+   */
+  randomizeCircle() {
+    const change = Math.floor(Math.random() * Math.floor(4));
+    let multiplier;
+
+    switch (change) {
+      case 0:
+        multiplier = smallInc;
+        break;
+      case 1:
+        multiplier = largeInc;
+        break;
+      case 2:
+        multiplier = smallDec;
+        break;
+      case 3:
+        multiplier = largeDec;
+        break;
+      default:
+        throw Error('Change must be a number between 0 and 3');
+    }
+
+    // Since we only change the PIXI object through scale we can't
+    // set it to a constant, hence only the else does something
+    if (this.arenaRadius * multiplier > maxRadius) {
+      // this.arenaRadius = maxRadius;
+    } else if (this.arenaRadius * multiplier < minRadius) {
+      // this.arenaRadius = minRadius;
+    } else {
+      this.arenaRadius *= multiplier;
+      this.mainCircle.scale.x *= multiplier;
+      this.mainCircle.scale.y *= multiplier;
     }
   }
 
