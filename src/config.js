a8584c7333568d661099dc3120cf52133192edd8
Tim Hakansson <timha404@liu.se>
2018-05-03 20:36:47 +0200

    Remove ds server from config

--- src/config.js
+++ src/config.js
@@ -14,7 +14,6 @@ const settings = {
     antialias: false,
   },
   communication: {
-    host_ip: 'ds.tddd96.i4demo.com:80',
     pingrate: 1,
     timeout_count: 5,
     service_name: 'tddd96client',
