8279f48943d2e24e121d8ffa560ca67cb1bcc7a7
Tim Hakansson <timha404@liu.se>
2018-05-03 20:40:14 +0200

    Change config id to gameclient

--- src/config.js
+++ src/config.js
@@ -16,9 +16,9 @@ const settings = {
   communication: {
     pingrate: 1,
     timeout_count: 5,
-    service_name: 'tddd96client',
+    service_name: 'gameclient',
     auth: {
-      id: 'tddd96client',
+      id: 'gameclient',
       password: '6da9475cfe8525232dfb42b293f56acea8333e4ee7ea56d961d9dbfe4d2ea324',
     },
   },
