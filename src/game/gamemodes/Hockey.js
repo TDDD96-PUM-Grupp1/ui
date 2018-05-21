724eda290150b11df3b34eb1def5e5dafcf74f9e
Tim Hakansson <timha404@liu.se>
2018-05-21 15:30:14 +0200

    Add descriptions to abilities

--- src/game/gamemodes/Hockey.js
+++ src/game/gamemodes/Hockey.js
@@ -206,6 +206,8 @@ class Hockey extends Gamemode {
       moveWhilePhased: false,
       rules: [
         'Play as a team and try to score by bumping the puck into the goal of the opposite team!',
+        'Abilities:',
+        'Speed Boost - Increases the speed of the played for more scoring potential!',
       ],
       leave: {
         removeTime: 3,
